c1c39988d752af33090757bd7f3d8adbb8caa7ed
artschedrov <art.worknsk@gmail.com>
2020-01-05 00:09:41 +0700

    05.01.20 add register, login, jwt, edit auth controller

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://ArtSchedrov:666123@cluster0-h9ood.mongodb.net/test?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://ArtSchedrov:666123@cluster0-h9ood.mongodb.net/test?retryWrites=true&w=majority',
+  jwt: 'dev-jwt'
 };
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,14 +1,54 @@
-module.exports.login = function (req, res) {
-  res.status(200).json({
-    login: {
-      email: req.body.email,
-      password: req.body.password
-    }
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys.js');
+const User = require('../models/User.js');
+
+module.exports.login = async function (req, res) {
+  const candidate = await User.findOne({
+    email: req.body.email
   });
+  if (candidate) {
+    const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+    if (passwordResult) {
+      const token = jwt.sign({
+        email: candidate.email,
+        userId: candidate._id
+      }, keys.jwt,{expiresIn: 60 * 60});
+      res.status(200).json({
+        token: `Bearer ${token}`
+      });
+    } else {
+      res.status(401).json({
+        message: 'Пароли не совпадают, проверьте правильность ввода'
+      })
+    }
+  } else {
+    res.status(404).json({
+      message: 'Пользователь с таким эмейлом не найден'
+    })
+  }
 };
 
-module.exports.register = function (req, res) {
-  res.status(200).json({
-    register: 'register from controller'
-  })
+module.exports.register = async function (req, res) {
+  const candidate = await User.findOne({
+    email: req.body.email
+  });
+  if (candidate) {
+    res.status(409).json({
+      message: 'Такой эмейл уже занят, попробуйте другой'
+    })
+  } else {
+    const salt = bcrypt.genSaltSync(10);
+    const password = req.body.password;
+    const user = new User({
+      email: req.body.email,
+      password: bcrypt.hashSync(password, salt)
+    });
+    try {
+      await user.save();
+      res.status(201).json(user)
+    } catch (e) {
+
+    }
+  }
 };
