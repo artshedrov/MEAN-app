f93130295ce372c58c04ee2bb4a84219d5d39b20
artschedrov <art.worknsk@gmail.com>
2020-01-12 18:57:37 +0700

    12.01.20 add API for categories

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,37 +1,73 @@
+const Category = require('../models/Category');
 const Position = require('../models/Position');
 const errorHandler = require('../utils/errorHandler');
 
 module.exports.getAllCategories  = async function (req, res) {
   try {
-    
+    const categories = await Category.find({
+      user: req.user.id
+    });
+    res.status(200).json(categories);
   } catch (e) {
    errorHandler(e, res);
   }
 };
 
-module.exports.getAllCategoryById = async function (req, res) {
-
-};
-
-module.exports.deleteCategoryById = function (req, res) {
+module.exports.getCategoryById = async function (req, res) {
   try {
-
+    const categories = await Category.findById({
+      user: req.params.id
+    });
+    res.status(200).json(categories);
   } catch (e) {
     errorHandler(e, res);
   }
 };
 
-module.exports.addCategory = function (req, res) {
+module.exports.deleteCategoryById = async function (req, res) {
   try {
-
+    await Category.remove({
+      _id: req.params.id
+    });
+    await  Position.remove({
+      category: req.params.id
+    });
+    res.status(200).json({
+      message: 'Категория удалена'
+    });
   } catch (e) {
     errorHandler(e, res);
   }
 };
 
-module.exports.updateCategory = function (req, res) {
+module.exports.addCategory = async function (req, res) {
+  const category = new Category({
+    name: req.body.name,
+    user: req.user.id,
+    imageSrc: req.file ? req.file.path : ''
+  });
   try {
+    await category.save();
+    res.status(201).json(category);
+  } catch (e) {
+    errorHandler(e, res);
+  }
+};
 
+module.exports.updateCategory = async function (req, res) {
+  const updated = {
+    name: req.body.name
+  };
+  if (req.file) {
+    updated.imageSrc = req.file.path
+  }
+  try {
+    const category = await Category.findOneAndUpdate(
+      {_id: req.params.id},
+      {$set: updated},
+      {new: true}
+      );
+    res.status(200).json(category);
   } catch (e) {
     errorHandler(e, res);
   }
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const controller = require('../controllers/category.controller.js');
 const passport = require('passport');
+const upload = require('../middleware/upload');
+const controller = require('../controllers/category.controller.js');
 const router = express.Router();
 
 
 router.get('/', passport.authenticate('jwt', {session: false}), controller.getAllCategories);
-router.get('/:id', controller.getAllCategoryById);
-router.delete('/:id', controller.deleteCategoryById);
-router.post('/', controller.addCategory);
-router.patch('/:id', controller.updateCategory);
+router.get('/:id', passport.authenticate('jwt', {session: false}), controller.getCategoryById);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), controller.deleteCategoryById);
+router.post('/', passport.authenticate('jwt', {session: false}), upload.single('image'), controller.addCategory);
+router.patch('/:id', passport.authenticate('jwt', {session: false}), upload.single('image'), controller.updateCategory);
 
 module.exports = router;
+
